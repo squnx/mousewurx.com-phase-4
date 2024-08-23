cfad78ce36d1b76e91ee2ac4b3a21f5b3b663ccf
Ivan Song <mousewurx@gmail.com>
2024-08-22 22:34:51 -0700

    removed space between :: and mousewurx link on the Footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
           {/* <a href=""><i className="bi bi-twitter-x"></i></a>
           <a href=""><i className="bi bi-facebook"></i></a>
           <a href=""><i className="bi bi-instagram"></i></a> */}
-          <a href="https://www.linkedin.com/in/ivansong/" title='LinkedIn' target="_blank" rel="noreferrer"><i className="bi bi-linkedin"></i></a>
+          <a href="https://www.linkedin.com/in/ivansong/" title="LinkedIn" target="_blank" rel="noreferrer"><i className="bi bi-linkedin"></i></a>
           {/* <a href="http://mousewurx.com/song/ivan/IvanSong.pdf" title="Iven's Resume" target='_blank'><i className="bi bi-file-earmark-person"></i></a> */}
         </div>
       </div>
