0e82410f71a10f8befc66ca809a6d2872af36286
Ivan Song <mousewurx@gmail.com>
2025-04-06 15:11:46 -0700

    Updated the title of 'AI-Powered Video Ad Creator' on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,8 +22,8 @@ const Home = () => {
         <img src="/assets/images/hero-ivan-222222.jpg" alt="" data-aos="fade-in" />
         <div className="container d-flex flex-column align-items-center justify-content-center text-center" data-aos="fade-up" data-aos-delay="100">
           <h2>Ivan Song <span className="d-sm-none"><br /></span><span>- from California</span></h2>
-          {/* <p><span className="typed" data-typed-items="Front End Engineer, Full-Stack Shopify Developer, AI-Generated Book Author, AI-Generated Illustrator"></span></p> */}
-          <p><span className="typed" data-typed-items="Front End Engineer, UI/UX Developer, AI-Generated Book Author, AI-Generated Illustrator"></span></p>
+          {/* <p><span className="typed" data-typed-items="Front End Engineer, AI-Powered Video Ad Creator, Full-Stack Shopify Developer, AI-Generated Book Author, AI-Generated Illustrator"></span></p> */}
+          <p><span className="typed" data-typed-items="Front End Engineer, UI/UX Developer, AI-Powered Video Ad Creator, Full-Stack Shopify Developer, AI-Generated Book Author, AI-Generated Illustrator"></span></p>
         </div>
       </section>
     </>
