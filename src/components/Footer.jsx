49cbe67e108ebdf42539d2633612fed8d282d4ff
Ivan Song <mousewurx@gmail.com>
2025-07-06 12:03:18 -0700

    Updated LinkedIn url from google.com on Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className="social-links d-flex justify-content-center">
           {/* <a href=""><i className="bi bi-twitter-x"></i></a> */}
           {/* <a href=""><i className="bi bi-instagram"></i></a> */}
-          <a href="https://www.linkedin.com/in/ivansong/" title="LinkedIn" target="_blank" rel="noreferrer"><i className="bi bi-linkedin"></i></a>
+          <a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.linkedin.com/in/ivansong&ved=2ahUKEwjwne6s86iOAxWYM0QIHfqhL1EQFnoECBgQAQ&usg=AOvVaw2DZ4wQUH0qQdFHUWpiHW-V" title="LinkedIn" target="_blank" rel="noreferrer"><i className="bi bi-linkedin"></i></a>
           <a href="https://www.facebook.com/MeoWoofPortraits" title="Facebook" target="_blank" rel="noreferrer"><i className="bi bi-facebook"></i></a>
           <a href="https://www.youtube.com/@squnx-media" title="YouTube" target="_blank" rel="noreferrer"><i className="bi bi-youtube"></i></a>
           {/* <a href="https://github.com/squnx?tab=repositories" title="GitHub" target="_blank" rel="noreferrer"><i className="bi bi-github"></i></a> */}
